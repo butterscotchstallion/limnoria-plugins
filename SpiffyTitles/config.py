aa7afe867a6c985087c8b9112a035af451fc3a26
Michael Telatynski <7t3chguy@gmail.com>
2015-05-30 15:34:35 +0100

    Tidied config.py

--- SpiffyTitles/config.py
+++ SpiffyTitles/config.py
@@ -28,99 +28,119 @@ def configure(advanced):
 SpiffyTitles = conf.registerPlugin('SpiffyTitles')
 
 conf.registerGlobalValue(SpiffyTitles, 'maxRetries',
-     registry.Integer(3, _("""Maximum retries upon failure""")))
+    registry.Integer(3, _("""Maximum Retries prior to Failure""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'wallClockTimeoutInSeconds',
-     registry.Integer(8, _("""Timeout for getting a title. If you set this too high, the bot will time out.""")))
+    registry.Integer(8, _("""Timeout for getting a title. If you set this too
+    high, the bot will time out.""")))
 
 # Language
 conf.registerGlobalValue(SpiffyTitles, 'language',
-     registry.String("en-US", _("""Language code""")))
+    registry.String("en-US", _("""Language Code""")))
 
 # imdb template
 conf.registerGlobalValue(SpiffyTitles, 'imdbTemplate',
-     registry.String("^ {{Title}} ({{Year}}, {{Country}}) - Rating: {{imdbRating}} ::  {{Plot}}", _("""Uses http://www.omdbapi.com to provide additional information about IMDB links""")))
+    registry.String("""^ {{Title}} ({{Year}}, {{Country}}) - Rating:
+    {{imdbRating}} ::  {{Plot}}""", _("""Uses http://www.omdbapi.com to provide
+    additional information about IMDB links""")))
 
 # enable/disable toggles
 conf.registerGlobalValue(SpiffyTitles, 'defaultHandlerEnabled',
-     registry.Boolean(True, _("""Whether to add additional information about regular links""")))
+    registry.Boolean(True, _("""Whether to add additional information about
+    Regular Links""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'youtubeHandlerEnabled',
-     registry.Boolean(True, _("""Whether to add additional information about YouTube videos""")))
+    registry.Boolean(True, _("""Whether to add additional information about
+    YouTube Videos""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'imgurHandlerEnabled',
-     registry.Boolean(True, _("""Whether to add additional information about imgur links""")))
+    registry.Boolean(True, _("""Whether to add additional information about
+    imgur Links""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'imdbHandlerEnabled',
-     registry.Boolean(True, _("""Whether to add additional information about IMDB links""")))
+    registry.Boolean(True, _("""Whether to add additional information about
+    IMDB Links""")))
 
 # URL regex
 conf.registerGlobalValue(SpiffyTitles, 'urlRegularExpression',
-     registry.String(r"(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})", _("""This regular expression will be used to match URLs""")))
+    registry.String(r"(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\."
+    r"[^\s]+\.[^\s]{2,})", _("""The regular expression used to match URLs""")))
 
 # Bold
 conf.registerGlobalValue(SpiffyTitles, 'useBold',
-     registry.Boolean(False, _("""Use bold in titles""")))
+    registry.Boolean(False, _("""Use bold in titles""")))
 
 # Title template
 conf.registerGlobalValue(SpiffyTitles, 'defaultTitleTemplate',
-     registry.String("^ {{title}}", _("""Template used for default title responses""")))
+    registry.String("^ {{title}}", _("""Template used for default title
+    responses""")))
 
 # YouTube template
 conf.registerGlobalValue(SpiffyTitles, 'youtubeTitleTemplate',
-     registry.String("^ {{title}} :: Duration: {{duration}} :: Views: {{view_count}} uploaded by {{channel_title}} :: {{like_count}} likes :: {{dislike_count}} dislikes :: {{favorite_count}} favorites", _("""Template used for YouTube title responses""")))
+    registry.String("""^ {{title}} :: Duration: {{duration}} :: Views:
+    {{view_count}} uploaded by {{channel_title}} :: {{like_count}} likes ::
+    {{dislike_count}} dislikes :: {{favorite_count}} favorites""",
+    _("""Template used for YouTube title responses""")))
 
 # User agents
 conf.registerGlobalValue(SpiffyTitles, 'userAgents',
-                        registry.CommaSeparatedListOfStrings(["Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.60 Safari/537.36", "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"], _("""Reported user agent when fetching links""")))
+    registry.CommaSeparatedListOfStrings(["""Mozilla/5.0 (Windows NT 6.1;
+    Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.60
+    Safari/537.36", "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101
+    Firefox/36.0", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS;
+    rv:11.0) like Gecko"""], _("""Reported user agent when fetching links""")))
 
 # Mime Types
 conf.registerGlobalValue(SpiffyTitles, 'mimeTypes',
-                        registry.CommaSeparatedListOfStrings(["text/html"], _("""Acceptable mime types for displaying titles""")))
+    registry.CommaSeparatedListOfStrings(["text/html"], _("""Acceptable MIME
+    Types for displaying titles""")))
 
 # Ignored domain pattern
 conf.registerGlobalValue(SpiffyTitles, 'ignoredDomainPattern',
-                        registry.Regexp("", _("""Domains matching this patterns will be ignored""")))
+    registry.Regexp("", _("""Domains matching this patterns will be
+    ignored""")))
 
 # Whitelist domain pattern
 conf.registerGlobalValue(SpiffyTitles, 'whitelistDomainPattern',
-                        registry.Regexp("", _("""Domains not matching this patterns will be ignored""")))
+    registry.Regexp("", _("""Domains not matching this patterns will be
+    ignored""")))
 
 # Channel whitelist
 conf.registerGlobalValue(SpiffyTitles, 'channelWhitelist',
-                        registry.CommaSeparatedListOfStrings([], _("""Only show titles on these channels, or all if empty""")))
+    registry.CommaSeparatedListOfStrings([], _("""Only show titles on these
+    channels, or all if empty""")))
 
 # Channel blacklist
 conf.registerGlobalValue(SpiffyTitles, 'channelBlacklist',
-                        registry.CommaSeparatedListOfStrings([], _("""Never show titles on these channels""")))
+    registry.CommaSeparatedListOfStrings([], _("""Never show titles on these
+    channels""")))
 
 # imgur API
 conf.registerGlobalValue(SpiffyTitles, 'imgurClientID',
-                        registry.String("", _("""imgur client ID""")))
+    registry.String("", _("""imgur Client ID""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'imgurClientSecret',
-                        registry.String("", _("""imgur client secret""")))
+    registry.String("", _("""imgur Client secret""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'imgurTemplate',
-                        registry.String("^{%if section %} [{{section}}] {% endif -%}{%- if title -%} {{title}} :: {% endif %}{{type}} {{width}}x{{height}} {{file_size}} :: {{view_count}} views :: {%if nsfw == None %}not sure if safe for work{% elif nsfw == True %}not safe for work!{% else %}safe for work{% endif %}", _("""imgur template""")))
+    registry.String("""^{%if section %} [{{section}}] {% endif -%}
+    {%- if title -%} {{title}} :: {% endif %}{{type}} {{width}}x{{height}}
+    {{file_size}} :: {{view_count}} views :: {%if nsfw == None %}not sure if
+    safe for work{% elif nsfw == True %}not safe for work!{% else %}safe for
+    work{% endif %}""", _("""imgur Template""")))
 
 conf.registerGlobalValue(SpiffyTitles, 'imgurAlbumTemplate',
-                        registry.String("^{%if section %} [{{section}}] {% endif -%}{%- if title -%} {{title}} :: {% endif %}{{image_count}} images :: {{view_count}} views :: {%if nsfw == None %}not sure if safe for work{% elif nsfw == True %}not safe for work!{% else %}safe for work{% endif %}", _("""imgur template""")))
+    registry.String("""^{%if section %} [{{section}}] {% endif -%}
+    {%- if title -%} {{title}} :: {% endif %}{{image_count}} images ::
+    {{view_count}} views :: {%if nsfw == None %}not sure if safe for work
+    {% elif nsfw == True %}not safe for work!{% else %}safe for work{% endif %}
+    """, _("""imgur template""")))
 
 # Youtube API
 conf.registerGlobalValue(SpiffyTitles, 'youtubeDeveloperKey',
-                        registry.String("", _("""Youtube developer key - required for Youtube handler.""")))
+    registry.String("", _("""Youtube Developer Key - required for Youtube
+    handler.""")))
 
 # Link cache lifetime
 conf.registerGlobalValue(SpiffyTitles, 'linkCacheLifetimeInSeconds',
-                        registry.Integer(60, _("""Link cache lifetime in seconds""")))                    
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
+    registry.Integer(60, _("""Link cache lifetime in seconds""")))
