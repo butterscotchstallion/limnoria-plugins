db076f3268c4d06dd104c40a9131212a2806afa8
Skeeter Alvarez <lunchdump@hellokitty.com>
2015-12-04 20:27:35 -0800

    Make limit configurable as constant, improve some readability

--- RelevantGif/plugin.py
+++ RelevantGif/plugin.py
@@ -25,15 +25,18 @@ except ImportError:
     _ = lambda x: x
 
 class RelevantGif(callbacks.Plugin):
-    """This plugin searches their elaborate and deep database
-    for a set of 10 images related to a given phrase, and selects one of them at
+    """This plugin related to a given phrase, and selects a result at
     random to return to the channel"""
     threaded = True
 
     def __init__(self, irc):
         self.__parent = super(RelevantGif, self)
         self.__parent.__init__(irc)
+
+        # This is a public beta API key
         self.GIPHY_API_KEY = "dc6zaTOxFJmzC"
+        # Setting to 0 always returns the first image
+        self.CHOOSE_FROM = 0
 
     def gif(self, irc, msg, args, query):
         """<phrase>
@@ -41,7 +44,7 @@ class RelevantGif(callbacks.Plugin):
         Returns a gif related to given phrase, Powered By Giphy!
         """
         encoded_query = quote_plus(query)
-        giphy_url = "http://api.giphy.com/v1/gifs/search?q=%s&api_key=%s&limit=10&offset=0" % (encoded_query, self.GIPHY_API_KEY)
+        giphy_url = "http://api.giphy.com/v1/gifs/search?q=%s&api_key=%s&limit=%d&offset=0" % (encoded_query, self.GIPHY_API_KEY, self.CHOOSE_FROM + 1)
         channel = msg.args[0]
         result = None
         headers = {
@@ -59,13 +62,18 @@ class RelevantGif(callbacks.Plugin):
 
                 total_count = response["pagination"]["total_count"]
                 no_result = total_count == 0
-                result_range_max = min(10, total_count)
 
                 if no_result:
                     self.log.info("RelevantGif: query error for %s" % (giphy_url))
                 else:
+                    if self.CHOOSE_FROM == 0:
+                        the_chosen_one = 0
+                    else:
+                        the_chosen_one = randrange(0, min(self.CHOOSE_FROM, total_count))
+
+                    chosen_url = response["data"][the_chosen_one]["images"]["original"]["url"]
                     relevantgif_template = self.registryValue("template")
-                    relevantgif_template = relevantgif_template.replace("$url", response["data"][randrange(0, result_range_max)]["images"]["original"]["url"])
+                    relevantgif_template = relevantgif_template.replace("$url", chosen_url)
 
                     result = relevantgif_template
             else:
